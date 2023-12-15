60005e02db24f8927ad634def79c30f21f6ca437
kimjh11130 <kimjh11130@naver.com>
2023-12-15 11:13:53 +0900

    1201

--- linux/ch11/assignment/assignment.c
+++ linux/ch11/assignment/assignment.c
@@ -18,32 +18,39 @@ void onerror(char *msg)
 
 int main()
 {
-	int p1[2], p2[2];
+	int p1[2], p2[2], p3[3];
 	char msg[MSGSIZE];
 	int i;
-	pid_t pid1, pid2;
+	pid_t pid1, pid2, pid3;
 	fd_set initset, newset;
 
-	pid1 = pid2 = 0;
+	pid1 = pid2 = pid3 = 0;
 
 	if(pipe(p1) == -1)
 		onerror("fail to call pipe() #1\n");
 	if(pipe(p2) == -1)
 		onerror("fail to call pipe() #2\n");
+	if(pipe(p3) == -1)
+		onerror("fail to call pipe() #3\n");
 
 	if((pid1 = fork()) == -1)
 		onerror("fail to call fork() #1\n");
 	if(pid1 > 0)
 		if((pid2 = fork()) == -1)
 			onerror("fail to call fork() #2\n");
+	if(pid1 > 0)
+		if(pid2 > 0)
+			if((pid3 = fork()) == -1)
+				onerror("fail to call fork() #3\n");
 
-	if(pid1 > 0 && pid2 > 0) {
+	if(pid1 > 0 && pid2 > 0 && pid3 > 0) {
 		printf("parent: %d\n", getpid());
-		close(p1[1]); close(p2[1]);
+		close(p1[1]); close(p2[1]); close(p3[1]);
 
 		FD_ZERO(&initset);
 		FD_SET(p1[0], &initset);
 		FD_SET(p2[0], &initset);
+		FD_SET(p3[0], &initset);
 
 		newset = initset;
 		while(select(p2[0] + 1, &newset, NULL, NULL, NULL) > 0) {
@@ -53,12 +60,15 @@ int main()
 			if(FD_ISSET(p2[0], &newset))
 				if(read(p2[0], msg, MSGSIZE) > 0)
 					printf("[parent] %s from child2\n", msg);
+			if(FD_ISSET(p3[0], &newset))
+				if(read(p3[0], msg, MSGSIZE) > 0)
+					printf("[parent] %s from child3\n", msg);
 			newset = initset;
 		}
 	}
-	else if(pid1 == 0 && pid2 == 0) {
+	else if(pid1 == 0 && pid2 == 0 && pid3 == 0) {
 		printf("child1: %d\n", getpid());
-		close(p1[0]); close(p2[0]); close(p2[1]);
+		close(p1[0]); close(p2[0]); close(p2[1]); close(p3[0]); close(p2[1]);
 
 		for(i = 0; i < 3; i++) {
 			sleep((i + 1) % 4);
@@ -68,9 +78,9 @@ int main()
 		printf("child1: bye!\n");
 		exit(0);
 	}
-	else if(pid1 > 0 && pid2 == 0) {
+	else if(pid1 > 0 && pid2 == 0 && pid3 == 0) {
 		printf("child2: %d\n", getpid());
-		close(p1[0]); close(p1[1]); close(p2[0]);
+		close(p1[0]); close(p1[1]); close(p2[0]); close(p3[0]); close(p3[1]);
 
 		for(i = 0; i < 3; i++) {
 			sleep((i + 3) % 4);
@@ -78,6 +88,18 @@ int main()
 			write(p2[1], "i'm child2", MSGSIZE);
 		}
 		printf("child2: bye!\n");
+		exit(0);
+	}
+	else if(pid1 > 0 && pid2 > 0 && pid3 == 0){
+		printf("child3: %d\n", getpid());
+		close(p1[0]); close(p1[1]); close(p2[0]); close(p2[1]); close(p3[0]);
+
+		for(i = 0; i < 3; i++){
+			sleep((i + 2) % 4);
+			printf("child3: send message %d\n", i);
+			write(p3[1], "i'm child3", MSGSIZE);
+		}
+		printf("child3: bye!\n");
 		exit(0);
 	}
 }
